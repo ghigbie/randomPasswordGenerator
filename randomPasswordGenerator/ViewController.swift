5adbc430e1369238ec5783ed0cf58ff31f2006f9
ghigbie <gehigbie@gmail.com>
2020-08-31 16:38:55 -0700

    Initial commit

---
+++ randomPasswordGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  randomPasswordGenerator
+//
+//  Created by George Higbie on 8/31/20.
+//  Copyright © 2020 trainingtrack.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
